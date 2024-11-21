88262a54b7862b9f2adccc72f8a5fd99c040fe64
rainn26 <xinzhao2627@gmail.com>
2024-11-21 18:57:31 +0800

    reportmodal

--- clientJs/src/components/ReportModal.jsx
+++ clientJs/src/components/ReportModal.jsx
@@ -18,6 +18,72 @@ import PropTypes from 'prop-types'
 import '@fontsource/inter'; 
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/600.css';
+import { TextareaAutosize as BaseTextareaAutosize } from '@mui/base/TextareaAutosize';
+import {padding, styled} from '@mui/system'
+import palette from '../assets/palette';
+function MaxHeightTextarea() {
+  const blue = {
+    100: '#DAECFF',
+    200: '#b6daff',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+  };
+
+  const grey = {
+    50: '#F3F6F9',
+    100: '#E5EAF2',
+    200: '#DAE2ED',
+    300: '#C7D0DD',
+    400: '#B0B8C4',
+    500: '#9DA8B7',
+    600: '#6B7A90',
+    700: '#434D5B',
+    800: '#303740',
+    900: '#1C2025',
+  };
+
+  const Textarea = styled(BaseTextareaAutosize)(
+    ({ theme }) => `
+    box-sizing: border-box;
+    width: 320px;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 8px 12px;
+    border-radius: 8px;
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+
+    &:hover {
+      border-color: ${blue[400]};
+    }
+
+    &:focus {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
+    }
+
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+  );
+
+  return (
+    <Textarea 
+        sx={{width:'100%'}}
+        maxRows={4}
+        aria-label="maximum height"
+        placeholder="Input your comment (optional)"
+    />
+  );
+}
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} {...props}/>;
 });
@@ -26,16 +92,17 @@ const ReportIconButton = ({ icon: Icon, label }) => (
     <IconButton 
         sx={{
             textAlign:'center', 
-            padding:0.3, 
-            borderRadius:'1px', 
-            border:'1px solid black', 
+            padding:0,  
+            borderRadius:0,
+            border:`1px solid ${palette.strokeMain}`, 
             width: '100%', 
-            height: '100%'
+            height: '40%',
+            color: palette.txtStrong
         }}
         disableRipple
     >
         
-        <Icon style={{height:'30px', width:'100%'}}/>
+        <Icon style={{height:'90%', width:'100%'}}/>
         
     </IconButton>
 );
@@ -47,12 +114,12 @@ ReportIconButton.propTypes = {
 const gridPreset = {
     xs:1,
     size:2.4,
-    marginBottom:2,
+    marginBottom:0,
     textAlign:'center',
  }
 
 const ReportGridItem = ({ icon: Icon, label }) => (
-    <Grid2 item {...gridPreset}>
+    <Grid2 {...gridPreset}>
         <ReportIconButton icon={Icon} label={label} />
         <Typography fontFamily={'Inter'} marginTop={0.5}>{label}</Typography>
     </Grid2>
@@ -184,6 +251,9 @@ const ReportModal = () => {
                             </Grid2>
                         </Stack>
                     </FormControl>
+
+                    <MaxHeightTextarea/>
+
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleSubmit} color="primary" sx={{ mt: 2 }}>
